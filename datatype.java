4a05a936ac9dbd7dc637dc8d618c4d689a417742
Vishakha1784 <vishakhashree4@gmail.com>
2025-08-11 11:38:24 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,22 @@
+public class datatype {
+        public static void main(String[] args) {
+            int age = 20;
+            float pi = 3.14f;
+            double d = 22.56;
+            char grade ='A';
+            boolean isPass = true;
+            byte b = 100;
+            short s = 32000;
+            long l = 123456789L;
+            System.out.println(age);
+            System.out.println(pi);
+            System.out.println(d);
+            System.out.println(grade);
+            System.out.println(isPass);
+            System.out.println(b);
+            System.out.println(s);
+            System.out.println(l);
+
+    }
+
+}
