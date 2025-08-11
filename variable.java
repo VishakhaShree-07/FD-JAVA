0eb4ae06d06c6ddb9fdd4b1463583e09888e9f5a
Vishakha1784 <vishakhashree4@gmail.com>
2025-08-11 10:48:18 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,13 @@
+public class variable {
+    public static void main(String[] args) {
+        int a = 7;
+        int _b = 9;
+        int $c = 29;
+        System.out.println(a);
+        System.out.println(_b);
+        System.out.println($c);
+
+
+    }
+    
+}
