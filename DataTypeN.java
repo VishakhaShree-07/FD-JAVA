07ee5b0b0565f50a253215fe9cc8d40bc1cc4037
Vishakha1784 <vishakhashree4@gmail.com>
2025-08-12 08:42:26 +0530

    Add files via upload

---
+++ DataTypeN.java
@@ -0,0 +1,14 @@
+public class DataTypeN {
+        public static void main(String[] args) {
+            String name = "Vishakha";
+            int []b = {3,6,7};
+            int c[] = {3,8,7};
+            int a[] = {3,3,7};
+            System.out.println(name);
+            System.out.println(b[1]);
+            System.out.println(c[0]);
+            System.out.println(a[2]);
+
+
+        }
+    }    
