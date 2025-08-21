d76e49fd408c83f3c90170361c5136c377627dc9
Vishakha1784 <vishakhashree4@gmail.com>
2025-08-21 13:48:14 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,20 @@
+public class arithmetic {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        int mod = a % b;
+        int div = a / b;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(mul);
+        System.out.println(mod);
+        System.out.println(div);
+
+
+    }
+}
